e1f087f02141c32724b80dafcf3f80fc18648837
Yusuf Karatas <yusufoemer.karatas@study.thws.de>
2026-05-15 17:24:34 +0200

    chore: wire dashboard api route

--- app/api/main.py
+++ app/api/main.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from app.api.routes.documents import router as documents_router
 from app.api.routes.entities import router as entities_router
 from app.api.routes.extractions import router as extractions_router
+from app.api.routes.dashboard import router as dashboard_router
 from app.api.vision import router as vision_router
 
 from app.db.database import get_db, initialize_vision_schema
@@ -13,7 +14,12 @@ app = FastAPI(title="EntityFlow API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,6 +28,7 @@ app.add_middleware(
 app.include_router(documents_router)
 app.include_router(entities_router)
 app.include_router(extractions_router)
+app.include_router(dashboard_router)
 app.include_router(vision_router)
 
 
